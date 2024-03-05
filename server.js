32053ef87035e5c2c9cd4a702dc34313e44cbb76
kien <lntk2221997@gmail.com>
2024-03-05 14:00:11 +0700

    zalo

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = 1337;
+const app = express();
+
+app.post('/zalo', (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
+})
+const server = app.listen(port, (error) => {
+    if (error) return console.log(`Error: ${error}`);
+    console.log(`Server listening on port: ${port}`);
+});
