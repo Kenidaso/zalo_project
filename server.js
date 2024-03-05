c86eb4de244c58a96f2ac813421946d182096969
kien <lntk2221997@gmail.com>
2024-03-05 15:51:21 +0700

    update zalo

--- server.js
+++ server.js
@@ -4,16 +4,14 @@ const port = process.env.PORT || 1337;
 const app = express();
 app.use(bodyParser.json());
 
-app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
-});
-
 app.post('/zalo', (req, res) => {
   console.log(req.body);
   res.json(req.body);
 });
+
+app.get('/', (req, res) => {
+    res.send("Hi");
+})
 const server = app.listen(port, (error) => {
     if (error) return console.log(`Error: ${error}`);
     console.log(`Server listening on port: ${port}`);
