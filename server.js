0e5c7b50edf91ae01cdb7a49b4bab90301ea3d53
kien <lntk2221997@gmail.com>
2024-03-06 14:03:55 +0700

    zalo_idsender

--- server.js
+++ server.js
@@ -4,14 +4,13 @@ const port = process.env.PORT || 1337;
 const app = express();
 app.use(bodyParser.json());
 
+
+
 app.post('/zalo', (req, res) => {
-  console.log(req.body);
+  console.log(req.body.sender);
   res.json(req.body);
 });
 
-app.get('/', (req, res) => {
-    res.send("Hi");
-})
 const server = app.listen(port, (error) => {
     if (error) return console.log(`Error: ${error}`);
     console.log(`Server listening on port: ${port}`);
